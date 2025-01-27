e5d2e5f001a89e2bf1c2cc24aa8a530993750014
Ruan <rugcosta1234@gmail.com>
2025-01-27 18:55:33 -0300

    fix: clean up database configuration by removing unnecessary comments

--- config/dataBase.js
+++ config/dataBase.js
@@ -11,8 +11,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect:process.env.DB_DIALECT,
-    port: 3307,//I was necessary to change the port baecause it was already in use the default: 3306, erase or change the port of your database
+    dialect: process.env.DB_DIALECT,
+
     logging: (msg) => console.log(msg),
   }
 );
